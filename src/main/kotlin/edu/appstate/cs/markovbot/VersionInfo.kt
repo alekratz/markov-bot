645844b2aa4225fc1a8e5117f20e58fc2b75c43b
Alek Ratzloff <alekratz@gmail.com>
2015-12-17 12:44:17 -0500

    Add VersionInfo file for compile-time version information

---
+++ src/main/kotlin/edu/appstate/cs/markovbot/VersionInfo.kt
@@ -0,0 +1,13 @@
+package edu.appstate.cs.markovbot
+
+/**
+ * @author Alek Ratzloff <alekratz@gmail.com>
+ */
+object VersionInfo {
+    const val MAJOR     = 0
+    const val MINOR     = 1
+    const val REV       = 0
+    const val SUFFIX    = "-alpha"
+
+    const val STR = "$MAJOR.$MINOR.$REV$SUFFIX"
+}
